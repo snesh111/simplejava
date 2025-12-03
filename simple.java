81012d88f67d18e7a70f0e5bac31224d2573bf1b
snesh <snesh6287@gmail.com>
2025-12-03 14:51:32 +0530

    added simple.java

---
+++ simple.java
@@ -0,0 +1,12 @@
+public class hello { // block
+            public static void main(String[] args)
+            {
+                System.out.println("hello java ");
+                System.out.println(3+2);
+                int age=12;
+                int AGE=18;  // case sen
+                int studentMarks=27; // camel case
+
+            }
+}
+
